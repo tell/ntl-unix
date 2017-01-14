6963d6722f0de59da4dd8aaa6e2b2fe8bad0abce
Tadanori TERUYA <tadanori.teruya@gmail.com>
2017-01-14 14:45:11 +0900

    Update common.mk

--- common.mk
+++ common.mk
@@ -3,7 +3,7 @@ PREFIX = $(ROOT)/prefix
 
 # Configurations for compilers:
 
-CXXFLAGS = -std=c++11 -O4 -g3 -Wall -Wextra -fsanitize=address -fsanitize=leak -fsanitize=undefined
+CXXFLAGS = -std=c++11 -O4 -g3 -Wall -Wextra # -fsanitize=address -fsanitize=leak -fsanitize=undefined
 ifeq (Linux,$(shell uname -s))
 CXXFLAGS += -pthread
 endif
